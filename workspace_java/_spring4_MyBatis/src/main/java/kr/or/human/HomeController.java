68c4973e6b0a1ad813fd51520f5d34d628d6d70a
이주희 <as106312@naver.com>
2025-04-07 17:06:49 +0900

    로그인 생성 및 다른거 했음

--- workspace_java/_spring4_MyBatis/src/main/java/kr/or/human/HomeController.java
+++ workspace_java/_spring4_MyBatis/src/main/java/kr/or/human/HomeController.java
@@ -26,6 +26,10 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
+		logger.error("error");
+		logger.warn("warning");
+		logger.debug("debug");
+		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
